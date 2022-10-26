7af27785c8affb311eceec8ad47a1eb481cc29c0
sanyam40 <sanyamnarang40@gmail.com>
2022-10-26 23:22:57 +0530

    JS

--- Js_Basics_1.js
+++ Js_Basics_1.js
@@ -80,3 +80,34 @@ else{
     console.log("NOT VALID")
     // alert("NOT VALID")
 }
+
+
+/* -------------------------------------------------
+
+-- LOOPS IN JS
+
+1) FOR LOOP */
+ for (let i= 0; i< 5;i++) {
+    console.log(i);
+}
+
+// 2) WHILE LOOP
+let a=5;
+
+while(a!=9){
+    console.log("HELLO","SANYAM")
+    a++;
+}
+
+// 3) FOR IN LOOP
+let obj={
+    sanyam:88,
+    arpita:89,
+    sahil:99
+}
+for(let a in obj){
+    console.log("Marks of "+a+" are "+obj[a])
+}
+
+
+
