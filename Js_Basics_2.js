6d7bb7905dcd1f6a5196b32e352dce08912b12d9
sanyam40 <sanyamnarang40@gmail.com>
2022-10-26 23:12:55 +0530

    JS

--- Js_Basics_2.js
+++ Js_Basics_2.js
@@ -1,6 +1,8 @@
 // Conditional expressions
 
-const prompt=require('prompt-sync')();
+// FOR INSTALLING PROMPT -- npm install prompt-sync
+
+/* const prompt=require('prompt-sync')();
 
 let age=prompt("ENTER AGE : ")
 if(age>=9){
@@ -9,12 +11,23 @@ if(age>=9){
 else{
     console.log("NOT VALID")
     // alert("NOT VALID")
-}
+} */ 
 
 // -------------------------------------------------
 
 // LOOPS IN JS
 
+// 1) FOR LOOP
+for (let i= 0; i< 5;i++) {
+    console.log(i);
+}
 
+// 2) WHILE LOOP
+let a=5;
+
+while(a!=9){
+    console.log("HELLO")
+    a++;
+}
 
 
