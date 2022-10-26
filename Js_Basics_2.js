3ab1a8e744d0c7e4c1b39aa60e9b82acc500ef61
sanyam40 <sanyamnarang40@gmail.com>
2022-10-26 20:48:09 +0530

    JS

---
+++ Js_Basics_2.js
@@ -0,0 +1,30 @@
+/* 
+   - PRIMITIVE DATA TYPES ARE SET OF BASIC DATA TYPES IN JS.
+   - Object is a non primitive datatype in js.
+
+   - There are the 7 primitve data types. ( NNBBSSU )
+   1)NULL 2)NUMBER 3)STRING 4)SYMBOL 5)UNDEFINED 6)BOOLEAN 7)BigInt
+
+*/
+
+let a=null
+let b=345
+let c="Sanyam"
+let d=Symbol("Hello Symbol")
+let e
+let f=true
+let g=BigInt(678)
+console.log(a,b,c,d,e,f,g)
+// FOR PRINTING DATA TYPE
+console.log(typeof a)
+
+// - OBJECT IN JS
+
+const item={
+    "Sanyam" : true,
+    "Sahil" : false,
+    "Vidit" : 67,
+    "Piyush" : undefined
+}
+console.log(item["Sanyam"])
+
