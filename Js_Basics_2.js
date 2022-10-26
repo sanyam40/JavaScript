29000496342272182b827cf4671df161540caeef
sanyam40 <sanyamnarang40@gmail.com>
2022-10-26 22:53:12 +0530

    JS

---
+++ Js_Basics_2.js
@@ -0,0 +1,11 @@
+// Conditional expressions
+
+const prompt=require('prompt-sync')();
+let age=prompt("ENTER AGE : ")
+
+if(age>=18){
+    console.log("VALID")
+}
+else{
+    console.log("NOT VALID")
+}
