d917cb1c5a0a440c65d9c76b884ed12cd0fff743
sanyam40 <sanyamnarang40@gmail.com>
2022-10-27 16:16:24 +0530

    JS

--- Js_Notes_Arrays.js
+++ Js_Notes_Arrays.js
@@ -1,11 +1,13 @@
 // ARRAYS IN JS
 
 const arr=[1,2,3,4]
-console.log(arr.length)
+const arr2=[5,6,7,8]
+
+console.log(arr.length) // RETURN THE ARRAY LENGTH
 console.log(arr)
 let a=arr.toString() // ARRAY TO STRING
 console.log(a)
-let c=arr.join(" and ")
+let c=arr.join(" and ") // ADDING B/W ELEMENT OF ARRAY
 console.log(c)
 console.log(arr.pop()) // POP - REMOVE FROM LAST AND RETURN LAST ELEMENT
 arr.push(8) // PUSH - ADDING ELEMENT AT LAST AND RETURN NEW ARRAY LENGTH
@@ -14,5 +16,18 @@ let d=arr.shift() // SHIFT - REMOVE THE RETURN THE FIRST ELEMENT
 console.log(arr,d)
 arr.unshift(22) // UNSHIFT - ADD ELEMENT AT FIRST AND RETURN THE NEW LENGTH
 console.log(arr)
-delete arr[0]
+delete arr[0] // DELETE THE ELEMENT AT SPECIFIC POS.
 console.log(arr)
+
+let array=arr.concat(arr2) // DOESN'T CHANGE EXISITNG ARRAY
+
+const  p=[9,8,7,1,11] 
+p.sort() // CHANGE THE ORIGINAL ARRAY -- ALPABATECIALLY
+console.log(p)
+p.reverse() // REVERSE THE ORIGINAL ARRAY
+console.log(p)
+
+p.splice(2,3,99,989,999) // TO REMOVE AND ADD ELEMENTS & IT RETURNS THE DELETED ELEMENTS
+console.log(p)
+
+console.log(p.slice(1)) // TO EXTRACT VALUES
